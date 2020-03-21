cec708b41603211d2855f76f6da3b7f06d96a532
haffff Gareou <bart99_10@o2.pl>
2020-03-21 15:05:43 +0100

    Next

--- MRasp3/Program.cs
+++ MRasp3/Program.cs
@@ -3,13 +3,16 @@ using System.IO;
 using System.Diagnostics;
 using System.Device.Gpio;
 using System.Linq;
+using System.Threading;
 
 namespace MRasp3
 {
     class Program
     {
         static Stopwatch stopwatch;
-        static FileSystemWatcher watcher = new FileSystemWatcher(File.Exists("path") ? File.ReadAllText("path") : "/media");
+        static Stopwatch songStopwatch;
+        static string path = File.Exists("path") ? File.ReadAllText("path") : "/media";
+        static FileSystemWatcher watcher = new FileSystemWatcher(path);
         static string[] songs;
         static int index = -1;
         static Process process = new System.Diagnostics.Process();
@@ -17,21 +20,40 @@ namespace MRasp3
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Working...");
             watcher.Created += HandlePendrive;
-            watcher.Deleted += HandlePendrive;
+            watcher.Deleted += ClearApp;
+            watcher.IncludeSubdirectories = true;
             watcher.EnableRaisingEvents = true;
             GpioController controller = new GpioController();
-            controller.RegisterCallbackForPinValueChangedEvent(3, PinEventTypes.Falling, (a,o) => Stopwatch.StartNew());
+            controller.OpenPin(3, PinMode.Input);
+            controller.OpenPin(5, PinMode.Input);
+            controller.OpenPin(7, PinMode.Input);
+            controller.OpenPin(11, PinMode.Input);
+            controller.RegisterCallbackForPinValueChangedEvent(3, PinEventTypes.Falling, (a,o) => { 
+                stopwatch = Stopwatch.StartNew();
+            });
             controller.RegisterCallbackForPinValueChangedEvent(3, PinEventTypes.Rising, TurnOff);
             controller.RegisterCallbackForPinValueChangedEvent(5, PinEventTypes.Rising, PlayStop);
             controller.RegisterCallbackForPinValueChangedEvent(7, PinEventTypes.Rising, Next);
             controller.RegisterCallbackForPinValueChangedEvent(11, PinEventTypes.Rising, Prev);
             process.Exited += GetNextSong;
-            while(programIsWorking)
-            {
-
-            }
+            Console.WriteLine("Subbed, starging loop...");
+            HandlePendrive(null,null);
+            while(programIsWorking) {}
+            Console.WriteLine("Exiting...");
+            controller.ClosePin(3);
+            controller.ClosePin(5);
+            controller.ClosePin(7);
+            controller.ClosePin(11);
             return;
+        }
+
+        private static void ClearApp(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("Clearing");
+            process.Kill();
+            index = -1;
         }
 
         private static void GetNextSong(object sender = null, EventArgs e = null)
@@ -53,12 +75,20 @@ namespace MRasp3
         }
 
 
-        private static void PlaySong(string song)
+        private static void PlaySong(string song,int from = 0)
         {
+            if(songStopwatch == null)
+            {
+                songStopwatch = Stopwatch.StartNew();
+            }
+            if(from == 0)
+                songStopwatch.Restart();
+            Console.WriteLine($"Playing song... {song}");
+            process = new Process();
             process.StartInfo = new ProcessStartInfo
             {
                 FileName = "/bin/bash",
-                Arguments = $"-c \"ffplay -vn -nodisp -autoexit\"",
+                Arguments = $"-c \"ffplay '{song}' -vn -autoexit -ss {from}\"", //-nodisp
                 RedirectStandardOutput = true,
                 UseShellExecute = false,
                 CreateNoWindow = true,
@@ -69,47 +99,61 @@ namespace MRasp3
 
         private static void Next(object sender, PinValueChangedEventArgs pinValueChangedEventArgs)
         {
+            Console.WriteLine($"checking index...");
             if (index < 0)
                 return;
-            process.Exited -= GetNextSong;
-            process.StandardInput.Write('q');
-            while (!process.HasExited) { }
+            Console.WriteLine($"Founded, Next()");
+            process.Kill();
+            Thread.Sleep(1000);
             GetNextSong();
-            process.Exited += GetNextSong;
         }
 
         private static void Prev(object sender, PinValueChangedEventArgs pinValueChangedEventArgs)
         {
+            Console.WriteLine($"checking index...");
             if (index < 0)
                 return;
-            process.Exited -= GetPrevSong;
-            process.StandardInput.Write('q');
-            while (!process.HasExited) { }
+            Console.WriteLine($"Founded, Prev()");
+            process.Kill();
+            Thread.Sleep(1000);
             GetPrevSong();
-            process.Exited += GetPrevSong;
         }
 
         private static void PlayStop(object sender, PinValueChangedEventArgs pinValueChangedEventArgs)
         {
+            Console.WriteLine($"checking index...");
             if (index < 0)
                 return;
-            process.StandardInput.Write('p');
+            if (songStopwatch != null && !songStopwatch.IsRunning)
+            {
+                PlaySong(songs[index], Convert.ToInt32(songStopwatch.Elapsed.TotalSeconds));
+                songStopwatch.Start();
+            }
+            if (songStopwatch != null && songStopwatch.IsRunning)
+            {
+                songStopwatch.Stop();
+                process.Kill();
+            }
+            Console.WriteLine($"Founded, Pause()");
         }
 
         private static void TurnOff(object sender, PinValueChangedEventArgs pinValueChangedEventArgs)
         {
-            if(stopwatch.Elapsed.TotalSeconds <= 3)
+            Console.WriteLine($"TimeEnd... {stopwatch.Elapsed.TotalSeconds}");
+            if (stopwatch.Elapsed.TotalSeconds <= 3)
             {
                 stopwatch.Stop();
-                stopwatch = null;
                 return;
             }
+            Console.WriteLine("Zzzz...");
+            Thread.Sleep(1000);
+            process.Kill();
             var p = new Process()
             {
                 StartInfo = new ProcessStartInfo
                 {
                     FileName = "/bin/bash",
-                    Arguments = $"-c \"shutdown\"",
+                    Arguments = $"-c \"shutdown -f\"",
                     RedirectStandardOutput = true,
                     UseShellExecute = false,
                     CreateNoWindow = true
@@ -121,16 +165,23 @@ namespace MRasp3
 
         private static void HandlePendrive(object sender, FileSystemEventArgs e)
         {
-            Scanner scanner = new Scanner(e.FullPath);
+            Console.WriteLine("Zzzz...");
+            Thread.Sleep(2000);
+            Console.WriteLine("Filewatcher starts working...");
+
+            Scanner scanner = new Scanner(path);
             songs = scanner.MusicFiles.ToArray();
+            Console.WriteLine($"Found {songs}");
             if (songs.Length > 0)
             {
                 index = 0;
                 PlaySong(songs[index]);
             }
             else
+            {
+                Console.WriteLine($"Not found, -1");
                 index = -1;
-            
+            }
         }
     }
 }
--- MRasp3/Scanner.cs
+++ MRasp3/Scanner.cs
@@ -13,12 +13,16 @@ namespace MRasp3
         public Scanner(string path)
         {
             MusicFiles = new List<string>();
+            Console.WriteLine("Stating scanner");
             lookFor(path);
         }
         private void lookFor(string path)
         {
+            Console.WriteLine("Working in -> " + path);
             var list = Directory.GetFiles(path).Where(x=>x.EndsWith(".mp3"));
+            Console.WriteLine("Added -> " + String.Join("\n",list));
             MusicFiles.AddRange(list);
+            Console.WriteLine(String.Join("\n", Directory.GetDirectories(path)));
             foreach (var i in Directory.GetDirectories(path))
             {
                 lookFor(i);
